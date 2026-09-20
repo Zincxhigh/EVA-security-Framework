4108919f2ce0a5b6ce41f641af81e1a59c7b5b33
TrulyVagabond <abubakarbinfaiz656@gmail.com>
2026-09-20 05:40:33 +0500

    Started working on the GUI of EVA: Security Framework

---
+++ GUI/app.py
@@ -0,0 +1,10 @@
+import customtkinter as ctk
+
+ctk.set_appearance_mode("dark")
+ctk.set_default_color_theme("green")
+
+app = ctk.CTk()
+app.geometry("1000x650")
+app.title("EVA: Security Framework")
+
+app.mainloop()
